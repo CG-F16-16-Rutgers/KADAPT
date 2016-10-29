9467244a79087198dd6459b79c3b426226d05534
cyrilmanayath <cyrilmanayath@users.noreply.github.com>
2016-10-29 19:18:11 -0400

    Cyril's first push

--- Assets/MyBehaviorTree2.cs
+++ Assets/MyBehaviorTree2.cs
@@ -26,35 +26,78 @@ public class MyBehaviorTree2 : MonoBehaviour
 
 	}
 
-	protected Node ST_ApproachAndWait(Transform target)
-	{
-		Val<Vector3> position = Val.V (() => target.position);
+    /*	protected Node ST_ApproachAndWait(Transform target)
+        {
+            Val<Vector3> position = Val.V (() => target.position);
 
-		//print("Sending BOW Command");
-		//return new Sequence( participant.GetComponent<BehaviorMecanim>().Node_BodyAnimation("BOW", true), new LeafWait(1000));
+            //print("Sending BOW Command");
+            return new Sequence( participant.GetComponent<BehaviorMecanim>().Node_BodyAnimation("BOW", true), new LeafWait(1000));
 
-		//print("Sending KARATEGREET Command");
-		//return new Sequence( participant.GetComponent<BehaviorMecanim>().Node_BodyAnimation("KARATEGREET", true), new LeafWait(1000));
+            //print("Sending KARATEGREET Command");
+            return new Sequence( participant.GetComponent<BehaviorMecanim>().Node_BodyAnimation("KARATEGREET", true), new LeafWait(1000));
 
-		//this was the original behaviour
-		return new Sequence( participant.GetComponent<BehaviorMecanim>().Node_GoTo(position), new LeafWait(1000));
-	}
+            //this was the original behaviour
+            return new Sequence( participant.GetComponent<BehaviorMecanim>().Node_GoTo(position), new LeafWait(1000));
+        }
 
-	protected Node BuildTreeRoot()
-	{
-		Val<float> pp = Val.V (() => police.transform.position.z);
-		Func<bool> act = () => (police.transform.position.z > 10);
-		Node roaming = new DecoratorLoop (
-			new Sequence(
-				this.ST_ApproachAndWait(this.wander1),
-				this.ST_ApproachAndWait(this.wander2),
-				this.ST_ApproachAndWait(this.wander3)));
-		Node trigger = new DecoratorLoop (new LeafAssert (act));
-		Node root = new DecoratorLoop (new DecoratorForceStatus (RunStatus.Success, new SequenceParallel(trigger, roaming)));
-		return root;
-		
+        protected Node BuildTreeRoot()
+        {
+            Val<float> pp = Val.V (() => police.transform.position.z);
+            Func<bool> act = () => (police.transform.position.z > 10);
+            Node roaming = new DecoratorLoop (
+                new Sequence(
+                    this.ST_ApproachAndWait(this.wander1),
+                    this.ST_ApproachAndWait(this.wander2),
+                    this.ST_ApproachAndWait(this.wander3)));
+            Node trigger = new DecoratorLoop (new LeafAssert (act));
+            Node root = new DecoratorLoop (new DecoratorForceStatus (RunStatus.Success, new SequenceParallel(trigger, roaming)));
+            return root;
 
-		//return roaming;
-	}
+
+            //return roaming;
+        }*/
+
+    protected Node ST_ApproachAndWait(Transform target)
+    {
+        Val<Vector3> position = Val.V(() => target.position);
+
+        //this was the original behaviour
+        return new Sequence(participant.GetComponent<BehaviorMecanim>().Node_GoTo(position), new LeafWait(1000));
+    }
+    protected Node ST_KarateGreet()
+    {
+
+        //print("Sending KARATEGREET Command");
+        return new Sequence(participant.GetComponent<BehaviorMecanim>().Node_BodyAnimation("KARATEGREET", true), new LeafWait(1000));
+    }
+    protected Node ST_Bow()
+    {
+        //Val<Vector3> position = Val.V(() => target.position);
+
+        //print("Sending BOW Command");
+        return new Sequence(participant.GetComponent<BehaviorMecanim>().Node_BodyAnimation("BOW", true), new LeafWait(1000));
+    }
+
+    protected Node BuildTreeRoot()
+    {
+        Val<float> pp = Val.V(() => police.transform.position.z);
+        Func<bool> act = () => (police.transform.position.z - participant.transform.position.z< 10);
+        Node roaming = //new DecoratorLoop(
+            new SequenceShuffle(
+                this.ST_ApproachAndWait(this.wander1),
+                this.ST_ApproachAndWait(this.wander2),
+                this.ST_ApproachAndWait(this.wander3),
+                this.ST_KarateGreet(),
+                this.ST_Bow());
+        Node trigger = new SequenceShuffle(new LeafAssert(act), this.ST_ApproachAndWait(this.wander1));
+        Node root = new DecoratorLoop(new DecoratorForceStatus(RunStatus.Success, new SequenceParallel(trigger, roaming)));
+        return root;
+
+
+        //return roaming;
+    }
+
+
+
 }
 
